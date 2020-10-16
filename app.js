06f71e713d6a2756c6063f7330a23eca5990c67e
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-15 22:23:38 -0700

    added name input

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions and grab DOM elements
+const name = document.getElementById('name');
+const bikeType = document.querySelectorAll(':checked');
 
+console.log(bikeType);
 // initialize state
 
 // set event listeners to update state and DOM
